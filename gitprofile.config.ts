29bae47f86ff2e26cfef8f1eb35ec6923aad12a8
Gari Arellano <gariiarellano01@gmail.com>
2024-06-04 20:25:45 +0200

    Update

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -222,7 +222,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'lofi',
+    defaultTheme: 'dracula',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
